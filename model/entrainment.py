2c10fbc022909ee0e67708fa55785869dafe78a5
Matt <mattm458@gmail.com>
2022-06-01 16:10:46 -0400

    Add entrainment model

---
+++ model/entrainment.py
@@ -0,0 +1,59 @@
+import pytorch_lightning as pl
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class FixedHistoryEntrainmentModel(pl.LightningModule):
+    def __init__(
+        self, lr=0.0001, rnn_in_dim=3, rnn_out_dim=32, out_dim=3, has_attention=False
+    ):
+        super().__init__()
+
+        if has_attention:
+            raise NotImplementedError("Attention not implemented")
+
+        self.lr = lr
+
+        self.rnn = nn.LSTM(rnn_in_dim, rnn_out_dim)
+        self.linear = torch.nn.Linear(rnn_out_dim, out_dim)
+
+    def forward(self, X):
+        if self.variable_batch_size:
+            X = nn.utils.rnn.pack_padded_sequence(X)
+
+        _, (h, _) = self.rnn(X)
+        h = h[-1]
+
+        out = self.linear(h)
+
+        return out
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
+
+    def training_step(self, batch, batch_idx):
+        X, y = batch
+        y_pred = self(X)
+
+        loss = F.mse_loss(y_pred, y)
+
+        self.log("train_loss", loss.detach(), on_epoch=True, on_step=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        X, y = batch
+        y_pred = self(X)
+
+        loss = F.smooth_l1_loss(y_pred, y)
+
+        self.log("val_loss", loss.detach(), on_epoch=True, on_step=True)
+
+        return loss
+
+    def predict_step(self, batch):
+        X, y = batch
+        y_pred = self(X)
+
+        return y_pred
